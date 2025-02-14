e1a8baad11302d80285a4dada0727569504d9ab1
Urvashi-Patel99 <urvashipatel87935@gmail.com>
2025-02-14 08:15:19 +0530

    Add files via upload

---
+++ StManagement/urls.py
@@ -0,0 +1,36 @@
+"""
+URL configuration for StManagement project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+
+# records/urls.py
+from django.urls import path
+from . import views
+urlpatterns = [
+ path('', views.StudentListView.as_view(), name='student_list'),
+ path('student/<int:pk>/', views.StudentDetailView.as_view(),
+name='student_detail'),
+ path('student/create/', views.StudentCreateView.as_view(),
+name='student_create'),
+ path('student/<int:pk>/edit/', views.StudentUpdateView.as_view(),
+name='student_edit'),
+ path('student/<int:pk>/delete/', views.StudentDeleteView.as_view(),
+name='student_delete'),
+ path('marks/create/', views.MarkCreateView.as_view(), name='mark_create'),
+ path('marks/<int:pk>/delete/', views.MarkDeleteView.as_view(),
+name='mark_delete'),
+]
+
